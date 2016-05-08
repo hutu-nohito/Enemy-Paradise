96637dd48e1a49eb0646fd220b4aee5ad05aeada
hutu-nohito <d0nte0sa@gmail.com>
2016-05-08 18:46:08 +0900

    モンスター　AI　簡略化

--- Assets/A_Assets/Character/monster/monster_Cont2.cs
+++ Assets/A_Assets/Character/monster/monster_Cont2.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class monster_Cont : Enemy_Parameter{
+public class monster_Cont2 : Enemy_Parameter
+{
 
     /******************************************************************************/
     /** @brief モンスターAIテスト
@@ -16,46 +17,19 @@ public class monster_Cont : Enemy_Parameter{
     */
     /******************************************************************************/
 
-
-    private Animator animator;
-    private int animState = 0;//アニメータのパラメタが取得できないのでとりあえずこれで代用
-    /*
-        アニメーションの番号割り振り
-
-        0   idle                            
-        1   walk
-        2   fight
-        3   attack
-
-    */                         
-
-    //汎用
-    private float time = 0;//使ったら戻す
-    private Coroutine coroutine;//一度に動かすコルーチンは1つ ここでとっとけば止めるのが楽
-    private bool isCoroutine = false;//コルーチンを止めるときにはfalseに戻すこと
-
-    private int priority = 0;//状態の優先度
-    /*
-    Stop    0
-    Walk    1
-    Fight   2
-    Attack  3
-    
-    */
-
     //キャラクタの状態
     public enum ActionState
     {
-        Stop,//止まっている状態
+        Stop,//アニメーションが終わるまで待機(状態でなく網目ーションの整合性のために必要)
+        Attack,//攻撃
         Walk,//プレイヤを見つけて近づいてる
         Fight,//臨戦態勢
-    }
+        
+    }//intにすれば優先度にできる
     public ActionState state = ActionState.Fight;
     public ActionState GetState() { return state; }
     public void SetState(ActionState state) { this.state = state; }
-
-    private ActionState oldstate = ActionState.Fight;
-
+    
     //追加中/////////////////////////////////////////////////////////
     private GameObject Player;
 
@@ -68,12 +42,8 @@ public class monster_Cont : Enemy_Parameter{
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
-
-
-        priority = 1;//最初はwalk
-
+        
         Player = GameObject.FindWithTag("Player");
-
     }
 
     // Update is called once per frame
@@ -83,20 +53,24 @@ public class monster_Cont : Enemy_Parameter{
         //アニメーションを取得してみる
         AnimatorStateInfo anim = animator.GetCurrentAnimatorStateInfo(0);
 
-        //動きが止まっている状態。アニメは前状態で決めとく
+        //何もしない
         if (state == ActionState.Stop)
         {
-            priority = 0;
+            //何もしない
+        }
+
+        //アタックするだけ
+        if (state == ActionState.Attack)
+        {
+            coroutine = StartCoroutine(Attack());
         }
 
         if (state == ActionState.Walk)
         {
-            priority = 1;
-
-            if (animState != 1)
+            
+            if (animState != 2)
             {
                 animator.SetTrigger("Run");
-                animState = 1;
             }
 
             //前を向ける
@@ -104,7 +78,7 @@ public class monster_Cont : Enemy_Parameter{
             transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
 
             iTween.MoveUpdate(this.gameObject, iTween.Hash(
-                "position", new Vector3(Player.transform.position.x,transform.position.y, Player.transform.position.z),
+                "position", new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z),
                 "time", 20 / speed)
                 );
 
@@ -120,12 +94,10 @@ public class monster_Cont : Enemy_Parameter{
 
         if (state == ActionState.Fight)
         {
-            priority = 2;
 
-            if (animState != 2)
+            if (animState != 3)
             {
                 animator.SetTrigger("Fight");
-                animState = 2;
             }
 
             //前を向ける
@@ -134,7 +106,7 @@ public class monster_Cont : Enemy_Parameter{
 
             //アフィン変換　回転
             Vector3 LocalPos = transform.position - Player.transform.position;//ターゲットに対するローカル座標に変換
-            Vector3 cal = new Vector3(0,transform.position.y,0);
+            Vector3 cal = new Vector3(0, transform.position.y, 0);
             float degreeTheta = -0.1f;
             degreeTheta = -speed * rotSpeed / LocalPos.magnitude * Time.deltaTime;//動かすときはスペックで違いが出ないようにDeltatime
             cal.x = LocalPos.x * Mathf.Cos(degreeTheta * Mathf.Deg2Rad) - LocalPos.z * Mathf.Sin(degreeTheta * Mathf.Deg2Rad);
@@ -153,7 +125,7 @@ public class monster_Cont : Enemy_Parameter{
             }
 
             //ランダムで攻撃
-            if((int)Time.time % 5 == 0)//5秒ごと
+            if ((int)Time.time % 5 == 0)//5秒ごと
             {
                 float randAt1 = Random.value;
                 float randAt2 = Random.value;
@@ -165,29 +137,24 @@ public class monster_Cont : Enemy_Parameter{
                     }
                 }
             }
-                        
         }
-
-        //状態が変化したら前の状態のいどうは中断
-        if (oldstate != state)
-        {
-            time = 0;
-            
-        }
-        oldstate = state;
-
     }
 
     /////////////////////////////////////
     //少し待ってから状態を遷移する
-    IEnumerator ChangeState(float waitTime , ActionState nextState)
+    IEnumerator ChangeState(float waitTime, ActionState nextState)
     {
         yield return new WaitForSeconds(waitTime);
 
         state = nextState;
     }
-    
+
     //イベントが起きた時/////////////////////
+
+    //汎用
+    private float time = 0;//使ったら戻す
+    private Coroutine coroutine;//一度に動かすコルーチンは1つ ここでとっとけば止めるのが楽
+    private bool isCoroutine = false;//コルーチンを止めるときにはfalseに戻すこと
 
     //攻撃
     IEnumerator Attack()
@@ -201,30 +168,50 @@ public class monster_Cont : Enemy_Parameter{
         yield return new WaitForSeconds(0.5f);//こーゆーパラメータもインスペクタで決めるべきだと思うけどごちゃごちゃしそうでいや
 
         animator.SetTrigger("Attack");
-        animState = 0;
         isCoroutine = false;
         coroutine = StartCoroutine(ChangeState(1.6f, ActionState.Fight));
-        
+
     }
 
     public void Damage()
     {
-        event_damage = true;
+        //イベント側で優先度を確認すればよい
+        if (state > ActionState.Fight)//これで臨戦態勢より高ければやってくれる
+        {
+            switch (state)//今の状態によって行動を変化
+            {
+                case ActionState.Attack:
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     //アニメーション
-    public void Idle()
-    {
-        animState = 0;
-    }
-
+    private Animator animator;
+    private int animState = 0;//アニメータのパラメタが取得できないのでとりあえずこれで代用
+    /*
+    アニメーションの番号割り振り          
+        1   attack
+        2   walk
+        3   fight
+    */
+    
     public void AnimAttack()
     {
-        Debug.Log("www");
-        animState = 0;
+        animState = 1;
     }
 
+    public void AnimRun()
+    {
+        animState = 2;
+    }
 
+    public void Animfight()
+    {
+        animState = 3;
+    }
 
     //武器を持つよう////////////////////////
     public Transform shield;
@@ -248,7 +235,7 @@ public class monster_Cont : Enemy_Parameter{
         weapon.parent = righthandpos;
         weapon.position = righthandpos.position;
         weapon.rotation = righthandpos.rotation;
-        
+
     }
 
     void holstershield()
@@ -256,9 +243,9 @@ public class monster_Cont : Enemy_Parameter{
         shield.parent = chestposshield;
         shield.position = chestposshield.position;
         shield.rotation = chestposshield.rotation;
-        
+
     }
-    
+
     void holsterweapon()
     {
         fightmodus = false;
