e356f1316d98c4aff4fc28abe9ac0906b5c743bb
hutu-nohito <d0nte0sa@gmail.com>
2017-08-20 21:03:11 +0900

    SteamSDK　導入

---
+++ Assets/A_Assets/Scripts/SteamTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class SteamTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (SteamManager.Initialized)
+        {
+            string name = SteamFriends.GetPersonaName();
+            Debug.Log(name);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
